82fea01b1338e926cfe0c89201bedf446e068ea6
pawelb92 <pawel.brzezinski92@gmail.com>
2022-10-25 20:17:09 +0200

    foreach cat

---
+++ src/Foreach.java
@@ -0,0 +1,10 @@
+public class Foreach {
+    public static void main(String[] args) {
+        String[] catTable = new String[]{"Gina", "kotlet", "Kazia","JUlian"};
+        for (String cat: catTable){
+            System.out.println(cat);
+        }
+
+
+    }
+}
